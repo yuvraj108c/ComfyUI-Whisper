4004d0f4a39962d96c24f1076e602df21d01e39f
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2024-01-27 17:55:20 +0400

    set fonts

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from .apply_whisper import ApplyWhisperNode
+from .add_subtitles_to_frames import AddSubtitlesToFramesNode
+from .add_subtitles_to_background import AddSubtitlesToBackgroundNode
+
+NODE_CLASS_MAPPINGS = { 
+    "Apply Whisper" : ApplyWhisperNode,
+    "Add Subtitles To Frames": AddSubtitlesToFramesNode,
+    "Add Subtitles To Background": AddSubtitlesToBackgroundNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+     "Apply Whisper" : "Apply Whisper", 
+     "Add Subtitles To Frames": "Add Subtitles To Frames",
+     "Add Subtitles To Background": "Add Subtitles To Background"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
